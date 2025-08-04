5c84faeb10c7915181431625c2ae1be436e751dd
Vaishali <kaushalvaishali1994@gmail.com>
2025-08-04 11:20:34 +0530

    build 4 aug

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "766311906"
+    serviceWorkerVersion: "3672390765"
   }
 });
